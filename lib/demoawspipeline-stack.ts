3344b47ba862e1a4338c8d580ba7c91deccca77d
Serkan <sersah95@gmail.com>
2023-05-16 12:43:58 +0300

    democicdpipeline added

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -1,12 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class DemoawspipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // AWS CI-CD Pipeline
+
+    const democicdpipeline = new CodePipeline(this,'demopipeline',
+{
+      synth: new ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: CodePipelineSource.gitHub('ser-2007/democicd', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });   
 
     // example resource
     // const queue = new sqs.Queue(this, 'DemoawspipelineQueue', {
