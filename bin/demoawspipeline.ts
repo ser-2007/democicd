eb6ce663b13d7359186e5934b73acd98b152c66f
Serkan <sersah95@gmail.com>
2023-05-16 13:59:07 +0300

    Update demoawspipeline.ts

--- bin/demoawspipeline.ts
+++ bin/demoawspipeline.ts
@@ -5,7 +5,7 @@ import { DemoawspipelineStack } from '../lib/demoawspipeline-stack';
 
 const app = new cdk.App();
 new DemoawspipelineStack(app, 'DemoawspipelineStack', {
-  env: { account: '503449184478', region: 'us-east-1' },
+  env: { account: 'aws-id', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
